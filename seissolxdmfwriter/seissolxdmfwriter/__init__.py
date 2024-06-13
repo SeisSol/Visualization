6fa357505f043ed8855dc0acb0a52f20152f64e4
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-06-13 21:47:09 +0200

    do the same for seissolxdmfwriter

--- seissolxdmfwriter/seissolxdmfwriter/__init__.py
+++ seissolxdmfwriter/seissolxdmfwriter/__init__.py
@@ -1,2 +1,11 @@
 from .seissolxdmfwriter import *
-__version__ = '0.4.1'
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("seissolxdmfwriter")
+except PackageNotFoundError:
+    # If the package is not installed, don't add __version__
+    pass
