60cc5279adcf7a85979f55c9e7ce5e29b0cd3820
David Schneller <12698011+davschneller@users.noreply.github.com>
2025-06-14 01:13:32 +0200

    Update seissolxdmfwriter.py

--- seissolxdmfwriter/seissolxdmfwriter/seissolxdmfwriter.py
+++ seissolxdmfwriter/seissolxdmfwriter/seissolxdmfwriter.py
@@ -3,7 +3,7 @@ import os
 from tqdm import tqdm
 import sys
 
-known_1d_arrays = ["locationFlag", "fault-tag", "partition", "clustering"]
+known_1d_arrays = ["locationFlag", "fault-tag", "partition", "clustering", "group", "boundary"]
 
 
 def dataLocation(prefix, name, backend):
